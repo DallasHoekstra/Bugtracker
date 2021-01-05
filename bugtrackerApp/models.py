21d4ea4185b9e7f9ac12a732e0cba6e4468940c6
Dallas Hoekstra <dallasjhoekstra@gmail.com>
2021-01-05 10:57:40 -0700

    Add Bug table to ORM

--- bugtrackerApp/models.py
+++ bugtrackerApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.fields.related import create_many_to_many_intermediary_model
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bug(models.Model):
+    title = models.CharField(max_length=200)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
