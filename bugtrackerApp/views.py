459d1f51cb16585659e39a94f7b23fb453d452a4
Dallas Hoekstra <dallasjhoekstra@gmail.com>
2021-01-05 11:04:09 -0700

    Change from static content to content queried from Bug table in database

--- bugtrackerApp/views.py
+++ bugtrackerApp/views.py
@@ -1,33 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'creator': 'Dev1',
-        'title:': 'dumb bug',
-        'content': 'This dumb bug occured',
-        'date_posted': 'August 1, 2000'
-    },
-
-    {
-        'creator': 'Dev1',
-        'title:': 'dumb bug 2',
-        'content': 'This dumb bug occured',
-        'date_posted': 'August 6, 2000'
-    },
-
-    {
-        'creator': 'Dev2',
-        'title:': 'dumb bug 3',
-        'content': 'This silly bug occured',
-        'date_posted': 'August 20, 2000'
-    }
-]
-
+from .models import Bug
 
 def home(request):
     # values inside context will be available to the templates
     context = {
-        'posts': posts
+        'bugs': Bug.objects.all()
     }
     return render(request, 'bugtrackerApp/home.html', context)
 
