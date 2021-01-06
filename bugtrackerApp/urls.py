ccf46f7ed0abdaeabf633d45d652c1f7dd6bb4ee
Dallas Hoekstra <dallasjhoekstra@gmail.com>
2021-01-06 12:53:30 -0700

    Add logout/login functionality, modify navbar on authentication state

--- bugtrackerApp/urls.py
+++ bugtrackerApp/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
+
+
 from . import views
-from users import views as user_views
+
+
 
 urlpatterns = [
     # django trims the already-matched parts of a url when working with the include function. An empty string means that the url accessed
     # was /bugtrackerapp
     path('', views.home, name='bugtrackerapp-home'),
-    path('about/', views.about, name='bugtrackerapp-about'),
-    path('register/', user_views.register, name='users-register')
+    path('about/', views.about, name='bugtrackerapp-about')
+    
 ]
 
 
