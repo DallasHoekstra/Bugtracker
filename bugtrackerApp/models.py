e0aa803e6129a75a70aa967d4d3a7f28285a4d1e
Dallas Hoekstra <dallasjhoekstra@gmail.com>
2021-01-13 15:17:33 -0700

    Add status table to models to represent the position of a bug in the workflow while allowing for creation of custom workflows.

--- bugtrackerApp/models.py
+++ bugtrackerApp/models.py
@@ -3,12 +3,21 @@ from django.db.models.fields.related import create_many_to_many_intermediary_mod
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+# Models flow from top to bottom. A model referenced by a class must appear before it.
+class Status(models.Model):
+    code = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=50, default="Unassigned")
+    description = models.TextField(default="This bug has not been assigned to a employee yet.")
+
+    def __str__(self):
+        return self.name
 
 class Bug(models.Model):
     title = models.CharField(max_length=200)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
     last_modified = models.DateTimeField(auto_now=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, default=2)
     description = models.TextField()
 
     def __str__(self):
@@ -16,3 +25,4 @@ class Bug(models.Model):
 
     def get_absolute_url(self):
         return reverse('bug-detail', kwargs={'pk': self.pk})
+
