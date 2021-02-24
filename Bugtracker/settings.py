443388e390666df2f46569cfa23ef923dfe09970
Dallas Hoekstra <dallasjhoekstra@gmail.com>
2021-02-23 17:15:27 -0700

    removed secrets from settings

--- Bugtracker/settings.py
+++ Bugtracker/settings.py
@@ -77,16 +77,7 @@ WSGI_APPLICATION = 'Bugtracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bugtrackerdb',
-        'USER': 'postgres',
-        'PASSWORD': 'MbPixma630',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
-}
+
 
 
 # Password validation
