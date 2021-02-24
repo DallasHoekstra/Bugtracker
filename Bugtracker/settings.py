33aa99a76bb3cf9fe71a4f9b221aeb9cc9455bbe
Dallas Hoekstra <dallasjhoekstra@gmail.com>
2021-02-24 10:49:29 -0700

    Removed DB secrets

--- Bugtracker/settings.py
+++ Bugtracker/settings.py
@@ -76,7 +76,16 @@ WSGI_APPLICATION = 'Bugtracker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USERNAME'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
+    }
+}
 
 
 
